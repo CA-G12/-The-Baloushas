42fd20bc4deb8c2ce425fbd69328eacfad5f599c
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-08-17 09:58:10 +0300

    fetch data from api in server side and send it to client side relates #9

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,7 @@
-console.log('sami')
+const btn = document.querySelector(`#test`);
+
+
+  fetch("/getMatches")
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
