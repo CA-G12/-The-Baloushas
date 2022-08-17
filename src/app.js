33808e91f8cbeb0686d595058a787825c633b54e
sam96B <same-96@hotmial.com>
2022-08-17 16:08:21 +0300

    handle fetching categorized matches from the server #17

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
-const compression = require('compression');
 const router = require('./routes');
 const { clientErrors, serverErrors } = require('./controllers/error');
 const app = express();
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 5500);
 
 
 app.use(express.json());
@@ -18,7 +17,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(router);
 app.use(router, (req, res, next) => {
-  console.log('Request Type:', req.method)
   next()
 })
 
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const {getMatches,getLeagueMatches} = require('./controllers')
 const router =require('express').Router()
 
 router.get('/getMatches',getMatches)
-router.get('/getLeagueMatches',getLeagueMatches)
+router.get('/getLeagueMatches/:name',getLeagueMatches)
 
 module.exports=router
 
