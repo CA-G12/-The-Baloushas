31b84c08c103fa9516136be45f67833fd4d0b1e4
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-08-17 16:40:39 +0300

    handle server error and clinet error relates #20

--- src/app.js
+++ src/app.js
@@ -1,24 +1,23 @@
-const path = require('path');
-const express = require('express');
-const helmet = require('helmet');
-const router = require('./routes');
-const { clientErrors, serverErrors } = require('./controllers/error');
+const path = require("path");
+const express = require("express");
+const helmet = require("helmet");
+const router = require("./routes");
+const { clientErrors, serverErrors } = require("./controllers/error");
 const app = express();
-app.set('port', process.env.PORT || 5500);
-
+app.set("port", process.env.PORT || 5500);
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
- app.use(
-   express.static(path.join(__dirname, '..', 'public'))
- );
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(router);
-app.use(router, (req, res, next) => {
-  next()
-})
+app.use(clientErrors);
+app.use(serverErrors);
 
+app.use(router, (req, res, next) => {
+  next();
+});
 
 module.exports = app;
