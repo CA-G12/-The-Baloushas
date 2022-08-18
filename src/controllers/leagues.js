fc3828d87ccddcc7c91810fa8b4bfe9955fe95c2
sam96B <same-96@hotmial.com>
2022-08-18 10:17:20 +0300

    handle js for next and prev buttons

--- src/controllers/leagues.js
+++ src/controllers/leagues.js
@@ -9,7 +9,6 @@ const url = "https://free-football-soccer-videos1.p.rapidapi.com/v1/";
 const options = {
   method: "GET",
   headers: {
-    // "X-RapidAPI-Key":  "8cb17f407cmsh5da2090b6ffbac0p13821ejsn48ad67b6ea9b",
     
     "X-RapidAPI-Key": process.env.API_KEY,
     "X-RapidAPI-Host": "free-football-soccer-videos1.p.rapidapi.com"
@@ -22,7 +21,7 @@ const getMatches = (req, res) => {
   fetch(url, options)
     .then((response) => response.json())
     .then((json) => {
-      json = json.slice(60, 75)
+      json = json.slice(50)
       res.json(json);
     })
     .catch((err) => console.error("error:" + err));
